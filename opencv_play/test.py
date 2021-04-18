a0a7028780500f77444bfdbef259500ff6b61a96
yoshimitsumatsutaIe <matsuta2718@gmail.com>
2021-04-19 00:34:23 +0900

    from ie

--- opencv_play/test.py
+++ opencv_play/test.py
@@ -1,44 +1,21 @@
-import cv2
-import sys
 
-camera_id = 0
-delay = 1
-window_name = 'frame'
 
-cap = cv2.VideoCapture(camera_id)
-
-if not cap.isOpened():
-    sys.exit()
-
-while True:
-    ret, frame = cap.read()
-    cv2.imshow(window_name, frame)
+class Tasu:
+    def __init__(self, a):
+        self.a = a
     
+    def tasu(self, b):
+        return self.a + b
+
+
+class Hiku:
+    def __init__(self, a):
+        self.a = a
     
-    
-    
-    
-    
-    if cv2.waitKey(delay) & 0xFF == ord('q'):
-        break
-
-cv2.destroyWindow(window_name)
+    def hiku(self, b):
+        return self.a - b
 
 
-
-
-
-import cv2
-
-aruco = cv2.aruco
-dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
-
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    cv2.imshow("frame", frame)
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(frame, dictionary) 
-    aruco.drawDetectedMarkers(frame, corners, ids) 
-    
-cap.release() 
-cv2.destroyAllWindows()
+class TasuHiku(Tasu, Hiku):
+    def __init__(self):
+        
