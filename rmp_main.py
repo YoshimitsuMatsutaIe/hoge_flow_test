d94df6368718b68b44ab1efb90b1be15a35afe74
Thoth <sbb03104@edu.osakafu-u.ac.jp>
2021-04-22 22:59:33 +0900

    from sc

---
+++ rmp_main.py
@@ -0,0 +1,19 @@
+import rmp
+
+class RMPalgebla(rmp.OriginalRMP):
+    """RMP代数？"""
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.goal = kwargs.pop('goal')
+        self.obs = kwargs.pop('obs')
+        self.jl = kwargs.pop('jl')
+    
+    
+    def push(self):
+        return None
+    
+    def pull(self):
+        return None
+    
+    def resolve(self):
